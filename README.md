a6f4c5ff9c136e1b0903afd7a92596952d4b9226
pazos07 <lucaspazos1@gmail.com>
2023-09-19 16:43:40 -0300

    Reentrega de pre-entrega 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Pre-entrega
-<!-- En este proyecto quise hacer un simulador de registro de pacientes como podria ser un consultorio. Intente hacerlo en bucle para que una vez cargado el formulario se pueda repetir, pero no pude implementarlo bien sin que se me rompa el condicional, lo deje comentado por si me pueden ayudar en la correccion de como implementarlo correctamente. La pagina html es basica tambien porque no hice el curso de WB y estoy aprendiendo sobre la marcha-->
+<!-- En este proyecto quise hacer un simulador de registro de pacientes como podria ser un consultorio.
+Primero hice una function para determinar la edad del registrado, para verificar si es mayor, menor o si la edad es invalida y asignarle una respuesta acorde a los datos proporcionados.
+Luego, se le solicitan los datos al usuario tal como nombre, apellido y edad. De esta manera el nombre y el apellido quedan registrados en la consola y se invoca a la funcion creada anteriormente para copilar los datos y devolverlos en forma de un alert con el mensaje especifico dado el rango de su edad.
+Finalmente, el bucle le pregunta al usuario si quiere hacer otra inscripcion, de responder "si" empieza de nuevo el registro y de otra manera, se rompe el bucle.
--- main.js
+++ main.js
@@ -1,37 +1,26 @@
 alert("¡Bienvenido a la inscripción!");
 
-function InscripciónDelPaciente() {
-  let nombre = document.getElementById("nombre").value;
-  let apellido = document.getElementById("apellido").value;
-  let edad = parseInt(document.getElementById("edad").value);
-  
+function InscripciónDelPaciente(nombre, apellido, edad) {
   if (!isNaN(edad)) {
     if (edad >= 18) {
       alert("Bienvenido " + apellido + ", " + nombre);
     } else if (edad >= 0) {
       alert("Bienvenido " + apellido + ", " + nombre + ". Tendrás que ser acompañado por un mayor de edad.");
     } else {
-      alert("Proporcione una edad válida.")
+      alert("Proporcione una edad válida.");
     }
   }
 }
 
-document.getElementById("formulario").addEventListener("submit", function (event) {
-  event.preventDefault();
-  InscripciónDelPaciente();
-});
+let respuestaDeUsuario = prompt("¿Quieres inscribir un paciente? (Si / No)");
 
+while (respuestaDeUsuario.trim().toLowerCase() === "si") {
+  const nombre = prompt("Ingresa el nombre del paciente:");
+  const apellido = prompt("Ingresa el apellido del paciente:");
+  const edad = parseInt(prompt("¿Qué edad tiene?"));
 
-// let continuar = true;
+  console.log(apellido + ", " + nombre);
+  InscripciónDelPaciente(nombre, apellido, edad);
 
-// while (continuar) {
-//   InscripciónDelPaciente();
-//   let respuesta = prompt("¿Desea realizar otra inscripción? Responder si o no");
-  
-//   if (respuesta && respuesta.trim().toLowerCase() === "si") {
-//     continuar = true;
-//   } else {
-//     continuar = false;
-//     break;
-//   }
-// }
+  respuestaDeUsuario = prompt("¿Quieres inscribir otro paciente? (Si / No)");
+}
