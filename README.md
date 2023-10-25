aa6960421674defd98220c74d3b629f820096b66
pazos07 <lucaspazos1@gmail.com>
2023-10-25 17:33:02 -0300

    Tercera Pre-Entrega

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Pre-entrega
-<!-- En este proyecto quise hacer un simulador de registro de pacientes como podria ser un consultorio.
+En este proyecto quise hacer un simulador de registro de pacientes como podria ser un consultorio. En primera instancia, solo se puede registrar x cantindad de pacientes pero en la entrega final pretendo tambien, ademas de la inscripcion de pacientes, hacer un simulador de reserva de turnos de manera que podria funcionar basicamente como una pagina de una clinica.
+
 Primero hice una function para determinar la edad del registrado, para verificar si es mayor, menor o si la edad es invalida y asignarle una respuesta acorde a los datos proporcionados.
 Luego, se le solicitan los datos al usuario tal como nombre, apellido y edad. De esta manera el nombre y el apellido quedan registrados en la consola y se invoca a la funcion creada anteriormente para copilar los datos y devolverlos en forma de un alert con el mensaje especifico dado el rango de su edad.
 Finalmente, el bucle le pregunta al usuario si quiere hacer otra inscripcion, de responder "si" empieza de nuevo el registro y de otra manera, se rompe el bucle.
 
 En esta segunda edicion de la entrega, agregué un objeto de cosntruccion donde los datos cargados a los prompts, aparecen registrados dentro de una tabla una vez terminada la carga de pacientes. A su vez un array permite ver el nombre de los pacientes en la consola de desarrollador. Y finalmente hay un nuevo boton de filtrado por edades para la tabla donde se puede filtrar a los pacientes con edades que no pertenezcan al grupo indicado.
+
+En esta tercera entrega, ya no se ingresan los datos de los pacientes mediante prompts, sino que ahora se registran mediante el DOM. Tambien una vez registrados los pacientes, si se refresca la pagina, quedan guardados mediante el local storage y se pueden purgar mediante un boton de limpieza que implemente para que no se acumulen pacientes de no ser necesario.
--- main.js
+++ main.js
@@ -1,77 +1,46 @@
-alert("¡Bienvenido a la inscripción!");
+alert("Bienvenido a la inscripción de pacientes.")
 
-function Paciente(nombre, apellido, edad) {
-  this.nombre = nombre;
-  this.apellido = apellido;
-  this.edad = edad;
-  this.toString = function() {
-    return this.apellido + ", " + this.nombre;
-  };
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const formulario = document.getElementById("formulario");
+  const tablaPacientes = document.getElementById("tablaPacientes");
+  let filtroEdad = null;
+  let pacientes = [];
 
-function InscripciónDelPaciente(paciente) {
-  if (!isNaN(paciente.edad)) {
-    if (paciente.edad >= 18) {
-      alert("Bienvenido " + paciente.apellido + ", " + paciente.nombre);
-    } else if (paciente.edad >= 0) {
-      alert("Bienvenido " + paciente.apellido + ", " + paciente.nombre + ". Tendrás que ser acompañado por un mayor de edad.");
+  const pacientesGuardados = localStorage.getItem("pacientes");
+  if (pacientesGuardados) {
+    pacientes = JSON.parse(pacientesGuardados);
+    pacientes.forEach(function (paciente) {
+      agregarPacienteATabla(paciente);
+    });
+  }
+
+  formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const edad = document.getElementById("edad").value;
+
+    if (nombre && apellido && edad) {
+      const paciente = {
+        nombre: nombre,
+        apellido: apellido,
+        edad: edad,
+      };
+
+      pacientes.push(paciente);
+      agregarPacienteATabla(paciente);
+
+      localStorage.setItem("pacientes", JSON.stringify(pacientes));
+
+      document.getElementById("nombre").value = "";
+      document.getElementById("apellido").value = "";
+      document.getElementById("edad").value = "";
     } else {
-      alert("Proporcione una edad válida.");
+      alert("Por favor, completa todos los campos del formulario.");
     }
-  }
-}
-
-let respuestaDeUsuario = prompt("¿Quieres inscribir un paciente? (Si / No)");
-const tablaPacientes = document.getElementById("tablaPacientes");
-const nombresPacientes = [];
-const pacientes = []; 
-
-while (respuestaDeUsuario !== null && respuestaDeUsuario.trim().toLowerCase() === "si") {
-  const nombre = prompt("Ingresa el nombre del paciente:");
-  const apellido = prompt("Ingresa el apellido del paciente:");
-  const edad = parseInt(prompt("¿Qué edad tiene?"));
-
-  if (!isNaN(edad)) {
-    const paciente = new Paciente(nombre, apellido, edad);
-    InscripciónDelPaciente(paciente);
-
-    const fila = tablaPacientes.insertRow();
-    const celdaNombre = fila.insertCell(0);
-    const celdaApellido = fila.insertCell(1);
-    const celdaEdad = fila.insertCell(2);
-
-    celdaNombre.textContent = paciente.nombre;
-    celdaApellido.textContent = paciente.apellido;
-    celdaEdad.textContent = paciente.edad;
-
-    nombresPacientes.push(paciente.nombre);
-    pacientes.push(paciente); 
-  } else {
-    alert("Por favor, ingrese una edad válida.");
-  }
-
-  respuestaDeUsuario = prompt("¿Quieres inscribir otro paciente? (Si / No)");
-
-  while (respuestaDeUsuario !== null && respuestaDeUsuario.trim().toLowerCase() !== "si" && respuestaDeUsuario.trim().toLowerCase() !== "no") {
-    alert("Por favor, responde con 'Si' o 'No'.");
-    respuestaDeUsuario = prompt("¿Quieres inscribir otro paciente? (Si / No)");
-  }
-}
-
-console.log("Nombres de los pacientes registrados:", nombresPacientes);
-
-document.getElementById("btnFiltrar").addEventListener("click", function() {
-  const filtroEdad = parseInt(document.getElementById("filtroEdad").value);
-
-  const pacientesFiltrados = pacientes.filter(function(paciente) {
-    return paciente.edad <= filtroEdad;
   });
 
-  while (tablaPacientes.firstChild) {
-    tablaPacientes.removeChild(tablaPacientes.firstChild);
-  }
-
-  pacientesFiltrados.forEach(function(paciente) {
+  function agregarPacienteATabla(paciente) {
     const fila = tablaPacientes.insertRow();
     const celdaNombre = fila.insertCell(0);
     const celdaApellido = fila.insertCell(1);
@@ -80,5 +49,41 @@ document.getElementById("btnFiltrar").addEventListener("click", function() {
     celdaNombre.textContent = paciente.nombre;
     celdaApellido.textContent = paciente.apellido;
     celdaEdad.textContent = paciente.edad;
+  }
+
+  function actualizarTabla() {
+    limpiarTabla();
+    pacientes.forEach(function (paciente) {
+      if (filtroEdad === null || paciente.edad == filtroEdad) {
+        agregarPacienteATabla(paciente);
+      }
+    });
+  }
+
+  function limpiarTabla() {
+    while (tablaPacientes.rows.length > 1) {
+      tablaPacientes.deleteRow(1);
+    }
+  }
+
+  document.getElementById("btnFiltrar").addEventListener("click", function () {
+    filtroEdad = parseInt(document.getElementById("filtroEdad").value);
+    actualizarTabla();
+  });
+
+  document.getElementById("btnResetFiltro").addEventListener("click", function () {
+    filtroEdad = null;
+    actualizarTabla();
+    document.getElementById("filtroEdad").value = "";
+  });
+
+  document.getElementById("btnLimpiarFormulario").addEventListener("click", function () {
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("edad").value = "";
+
+    localStorage.removeItem("pacientes");
+
+    limpiarTabla();
   });
 });
